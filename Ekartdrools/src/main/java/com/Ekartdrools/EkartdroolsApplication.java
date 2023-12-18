79d5cfa5184f9f296969f88a496009894ab37635
ATIF(thor) <aatif1317ghazi@gmail.com>
2023-12-18 22:27:13 +0530

    DYnamic SQL SPRINGboot  commit

---
+++ Ekartdrools/src/main/java/com/Ekartdrools/EkartdroolsApplication.java
@@ -0,0 +1,13 @@
+package com.Ekartdrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartdroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkartdroolsApplication.class, args);
+	}
+
+}
---
+++ cart/src/main/java/com/cart/CartApplication.java
@@ -0,0 +1,16 @@
+package com.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartApplication.class, args);
+	}
+
+}
+
+
+//https://youtu.be/RfHBm5yRxuw?list=PLcs1FElCmEu121gqGwlQt47d0SqNkzSTK
